64ce4840443687bc87b8552d2af8396606da6095
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-04-15 10:30:58 -0600

    update server code

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const app = require("express") ();
 const http = require("http").createServer( app );
 const io = require('socket.io') (http, {
-  // cors: { origin: 'http://localhost:3000'}
   cors: { origin: 'https://loophoria.herokuapp.com'} // this will become the heroku server link url
+//  cors: { origin: "http://localhost:3000" }
 });
+
+
 
 io.on('connection', socket => {
   console.log('connected');
-  socket.on('send_message', ( src ) => {
-    socket.broadcast.emit("message", src);
+  socket.on('send_message', (src, index, button) => {
+    socket.broadcast.emit("message", src, index, button);
   });
-  socket.on("stop_message", (src) => {
-    socket.broadcast.emit("stop_play", src);
+  socket.on("stop_everyone", (src, index, button) => {
+    socket.broadcast.emit("stop_play", src, index, button);
   }); 
 });
+
+
 
 http.listen(4000, function () {
   console.log("listening on port 4000");
 });
+
+
+
+
+
 
