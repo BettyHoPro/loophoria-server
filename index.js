12a835469129c47309ce44a45ed69745d326d4be
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-04-16 15:05:07 -0600

    server updates, comented out

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const app = require("express") ();
 const http = require("http").createServer( app );
 const io = require('socket.io') (http, {
-  cors: { origin: 'https://loophoria.herokuapp.com'} // this will become the heroku server link url
-//  cors: { origin: "http://localhost:3000" }
+//  cors: { origin: 'https://loophoria.herokuapp.com'} // heroku-client URL
+  cors: { origin: "http://localhost:3000" } // local-client URL
 });
 
 
 
 io.on('connection', socket => {
   console.log('connected');
+
+  // let enableButtons = [{user: user, buttons: buttons}];
+
+
+
   socket.on('send_message', (src, index, button) => {
     socket.broadcast.emit("message", src, index, button);
   });
   socket.on("stop_everyone", (src, index, button) => {
     socket.broadcast.emit("stop_play", src, index, button);
-  }); 
+  });
+
+  // socket.on("enable_buttons", (buttonsInUse) => {
+  //   console.log("CIENT DISCONNECTED");
+  //   socket.broadcast.emit("client_disconnected", buttonsInUse);
+  // }); 
+  //  socket.on("disconnect", () => {
+  //   console.log("CIENT DISCONNECTED");
+  //   socket.broadcast.emit("client_disconnected", enableButtons);
+  // }); 
+
 });
 
 
